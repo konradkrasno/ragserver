dd09f4d199bb55d0370632071a796e479b88ba7d
konradkrasno <konradkrasno@gmail.com>
2024-12-27 15:05:15 +0100

    Stream response

--- src/rag/rag.go
+++ src/rag/rag.go
@@ -12,7 +12,7 @@ import (
 	"github.com/tmc/langchaingo/llms/ollama"
 	"github.com/tmc/langchaingo/schema"
 	"github.com/tmc/langchaingo/vectorstores/weaviate"
-	"log"
+	"hash/fnv"
 	"strings"
 )
 
@@ -79,10 +79,11 @@ func (rs *Rag) AddDocuments(adr models.AddDocumentsRequest) error {
 	return nil
 }
 
-func (rs *Rag) query(qr models.QueryRequest) (string, error) {
+func (rs *Rag) Query(qr models.QueryRequest) {
 	docs, err := rs.WvStore.SimilaritySearch(rs.Ctx, qr.Content, rs.Envs.WvDocumentsRetrievalNumber)
 	if err != nil {
-		return "", err
+		fmt.Println(err)
+		return
 	}
 	docContents := make([]string, len(docs))
 	for i, doc := range docs {
@@ -90,29 +91,32 @@ func (rs *Rag) query(qr models.QueryRequest) (string, error) {
 	}
 
 	ragQuery := fmt.Sprintf(ragTemplateStr, qr.Content, strings.Join(docContents, "\n"))
-	return llms.GenerateFromSinglePrompt(rs.Ctx, rs.LLMClient, ragQuery, llms.WithModel(rs.Envs.LLM))
+	hashedQuery := hash(ragQuery)
+	_, err = llms.GenerateFromSinglePrompt(
+		rs.Ctx,
+		rs.LLMClient,
+		ragQuery,
+		llms.WithModel(rs.Envs.LLM),
+		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			item := make(map[string]interface{})
+			item["id"] = hashedQuery
+			item["content"] = chunk
+			data, err := json.Marshal(item)
+			if err != nil {
+				return err
+			}
+
+			return rs.Broker.Publish(rs.Envs.RabbitMQAnswerExchange, qr.SessionId, data)
+		}),
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
-func (rs *Rag) Query(qr models.QueryRequest) {
-	answer, err := rs.query(qr)
-	if err != nil {
-		log.Println(err)
-		answer = fmt.Sprintf("error occurred: %s", err)
-	}
-
-	resp := models.QueryResponse{
-		Query:  qr.Content,
-		Answer: answer,
-	}
-	data, err := json.Marshal(resp)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	err = rs.Broker.Publish(rs.Envs.RabbitMQAnswerExchange, qr.SessionId, data)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
